ee243095b145e0bbdc73f2a131c588404455d391
Ali Gohar <aligoharkp@gmail.com>
2023-07-19 13:53:19 +0500

    Loops

--- Program.cs
+++ Program.cs
@@ -1,25 +1,50 @@
 ﻿using System;
-namespace datatypes{
-    class Program{
-        static void Main(){
+namespace Array
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            int[] marks = {0,1,2,3,4,5,6,7,8,9 };
+            int i = 0;
 
-        float percentage = 17.7F;
-        Console.WriteLine(percentage);
-        string name = "Jalal";
-        Console.WriteLine("string is " +name);
-        int marks=67;
-        Console.WriteLine(marks);
-        char shemale = 'S';
-        Console.WriteLine("char is = "+shemale);
-        float Area = 20.5F;
-        //asdasdasdasdasdas
-        Console.WriteLine(Area);
+            foreach(var j in marks)
+            {
+                Console.WriteLine(j);
 
+            }
+        
+            while( i<10)
+            {
+                Console.WriteLine(marks[i]);
+                i++;
+
+            } 
+
+                int x =0;
+                x = ++x;
+                Console.WriteLine(x);
+                int y =0;
+                y =++y;
+                Console.WriteLine(y); 
+
+                var Name = 12;
+                Console.WriteLine(Name);
+                
+
+
+        
+            
 
 
         }
 
 
 
-    }    
+    }
+
+
+
+
 }
