3aa3bfeb2691bb05b6f31d83890598a2d3077267
Ali Gohar <aligoharkp@gmail.com>
2023-07-19 12:20:56 +0500

    this is my first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace datatypes{
+    class Program{
+        static void Main(){
+
+        float percentage = 17.7F;
+        Console.WriteLine(percentage);
+        string name = "Jalal";
+        Console.WriteLine("string is " +name);
+        int marks=67;
+        Console.WriteLine(marks);
+        char shemale = 'S';
+        Console.WriteLine("char is = "+shemale);
+        float Area = 20.5F;
+        Console.WriteLine(Area);
+
+
+
+        }
+
+
+
+    }    
+}
