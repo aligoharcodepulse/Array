2dd9210911537ce00185a3e6e7793cd7fe1ebf40
Ali Gohar <aligoharkp@gmail.com>
2023-07-20 13:11:46 +0500

    Index

--- Program.cs
+++ Program.cs
@@ -4,35 +4,19 @@ namespace Array
     class Program
     {
         static void Main()
-        {
-            
-            int[] marks = {0,1,2,3,4,5,6,7,8,9 };
-            int i = 0;
+        {    
 
-            foreach(var j in marks)
+            int[] Numbers= {1,2,3,4,5};
+            int sum = 0;
+            for(int i = 0; i < 5 ; i++  )
             {
-                Console.WriteLine(j);
-
+                Console.WriteLine("At index"+i);
+                 Console.WriteLine("our value is = "+Numbers[i]);
+              
             }
-        
-            while( i<10)
-            {
-                Console.WriteLine(marks[i]);
-                i++;
-
-            } 
-
-                int x =0;
-                x = ++x;
-                Console.WriteLine(x);
-                int y =0;
-                y =++y;
-                Console.WriteLine(y); 
-
-                var Name = 12;
-                Console.WriteLine(Name);
+      
+            
                 
-
 
         
             
